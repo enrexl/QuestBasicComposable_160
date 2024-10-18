daddd60f8d7a07fd1402731e84e09192d3d39aa2
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-18 19:12:16 +0700

    Add image roundcorner clip into potrait.png

--- app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
+++ app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,10 +69,13 @@ Column(
         fontSize = 25.sp,
         fontWeight = FontWeight.Bold)
 
-Spacer(modifier = Modifier.padding(10.dp))
+    Spacer(modifier = Modifier.padding(10.dp))
 
     Image(painter = painterResource(id = R.drawable.potrait)
-    , contentDescription = "")
+    , contentDescription = "",
+        modifier = Modifier.clip(RoundedCornerShape(16.dp))
+    )
+
 
 }
 }
