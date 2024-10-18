4542618130dc8a38943f52bd09580fa3a7978e53
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-18 18:37:45 +0700

    Add vertical and horizontal alignment for "BasicColumn" function

--- app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
+++ app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.week3_questcomposable.ui.theme.Week3QuestComposableTheme
@@ -31,7 +33,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicColumn(modifier: Modifier = Modifier){
-Column() {
+Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Top,
+
+) {
     Text("Login")
     Text("Ini Adalah Halaman Login")
 }
