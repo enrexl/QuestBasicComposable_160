84ffdbcfaaf0e72720ed05dc0b79e6da9ad83339
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-18 18:43:51 +0700

    Add logo "logo.png" into "BasicColumn"

--- app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
+++ app/src/main/java/com/example/week3_questcomposable/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +45,8 @@ Column(
 ) {
     Text("Login")
     Text("Ini Adalah Halaman Login")
+    Image(painter = painterResource(id = R.drawable.logo)
+    , contentDescription = "")
 }
 }
 
